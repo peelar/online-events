0539bb3467550a74506b220d0eba2c85e7fdb1a6
Adrian Pilarczyk <adrian@useo.pl>
2021-03-22 00:01:34 +0100

    add: test ui-lib

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Button } from 'ui-lib'
+import { Button, Text } from 'ui-lib'
 
 const Index: React.FC = () => {
   return (
-    <div>
-      Hello
+    <div style={{ margin: '0 auto', maxWidth: '1080px', padding: '2rem' }}>
+      <Text fontSize="xl">Hello!</Text>
       <Button>I love Gabi ❤️</Button>
     </div>
   )
