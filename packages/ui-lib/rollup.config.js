5b9b70e83cbbc9e8f84433461e964f2392fa419f
Adrian Pilarczyk <adrian@useo.pl>
2021-03-22 19:05:28 +0100

    change: cleanup

--- packages/ui-lib/rollup.config.js
+++ packages/ui-lib/rollup.config.js
@@ -1,9 +1,6 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
 import url from 'rollup-plugin-url'
-
 import pkg from './package.json'
 
 export default {
@@ -25,34 +22,9 @@ export default {
   plugins: [
     external(),
     url(),
-    resolve({
-      customResolveOptions: {
-        moduleDirectory: 'packages/*',
-      },
-    }),
     typescript({
       rollupCommonJSResolveHack: true,
       clean: true,
-    }),
-    commonjs({
-      include: /node_modules/,
-      // left-hand side can be an absolute path, a path
-      // relative to the current directory, or the name
-      // of a module in node_modules
-      namedExports: {
-        'node_modules/react/index.js': [
-          'cloneElement',
-          'createContext',
-          'Component',
-          'createElement',
-        ],
-        'node_modules/react-dom/index.js': ['render', 'hydrate'],
-        'node_modules/react-is/index.js': [
-          'isElement',
-          'isValidElementType',
-          'ForwardRef',
-        ],
-      },
     }),
   ],
 }
