a61592dbef65e77a30544a3574dd08690364e1bd
Adrian Pilarczyk <adrian@useo.pl>
2021-03-22 20:52:36 +0100

    add: run all in parallel script

--- packages/ui-lib/src/components/Button/Button.tsx
+++ packages/ui-lib/src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button as CHButton } from '@chakra-ui/react'
 
 const Button: typeof CHButton = (props) => {
-  return <CHButton colorScheme="pink" {...props} />
+  return <CHButton {...props} />
 }
 
 export default Button
