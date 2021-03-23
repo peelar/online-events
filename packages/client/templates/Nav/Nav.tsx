cf3bb635f8c419788ae5342032da008e0d4252b2
Adrian Pilarczyk <adrian@useo.pl>
2021-03-23 18:10:24 +0100

    add: Nav

---
+++ packages/client/templates/Nav/Nav.tsx
@@ -0,0 +1,15 @@
+import User from "features/User/User";
+import React from "react";
+import { Box, Flex } from "ui-lib";
+
+const Nav: React.FC = () => {
+  return (
+    <Box py={2} fullWidth>
+      <Flex direction="row-reverse">
+        <User />
+      </Flex>
+    </Box>
+  );
+};
+
+export default Nav;
