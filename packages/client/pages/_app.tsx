5c50405d5eae6ba5311d1f28a48a5c57c2ec6828
Adrian Pilarczyk <adrian@useo.pl>
2021-03-23 18:07:37 +0100

    add: configure next-auth

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import Layout from "templates/Layout/Layout";
 import { ChakraProvider } from "ui-lib";
+import { Provider } from "next-auth/client";
+import { AppProps } from "next/dist/next-server/lib/router/router";
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Provider session={pageProps.session}>
+          <Component {...pageProps} />
+        </Provider>
+      </Layout>
     </ChakraProvider>
   );
-}
+};
 
 export default MyApp;
