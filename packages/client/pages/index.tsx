daec065e8c3afac8540bc654a325af908f608e01
Adrian Pilarczyk <adrian@useo.pl>
2021-03-23 18:07:58 +0100

    change: replace div with style in pages/index

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Button, Text } from "ui-lib";
+import { Button, Text, Box } from "ui-lib";
 
 const Index: React.FC = () => {
   return (
-    <div style={{ margin: "0 auto", maxWidth: "1080px", padding: "2rem" }}>
+    <Box>
       <Text fontSize="xl">Hello!</Text>
       <Button>I love Gabi ❤️</Button>
-    </div>
+    </Box>
   );
 };
 
