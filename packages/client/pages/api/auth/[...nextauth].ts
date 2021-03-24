6035b10b08f62a210d609335903f9e196ce96952
Adrian Pilarczyk <adrian@useo.pl>
2021-03-24 20:50:05 +0100

    add: Google requests auth

--- packages/client/pages/api/auth/[...nextauth].ts
+++ packages/client/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,20 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
 export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      scope:
+        "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar",
+      authorizationUrl:
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+    }),
+  ],
+  jwt: {
+    encryption: true,
+  },
+  secret: process.env.SECRET,
   callbacks: {
     async jwt(token, _, account) {
       if (account?.accessToken) {
@@ -10,12 +24,4 @@ export default NextAuth({
       return token;
     },
   },
-  providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      scope:
-        "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar",
-    }),
-  ],
 });
