518dde6262520d00259025d38ed80ab82da090d8
Adrian Pilarczyk <adrian@useo.pl>
2021-03-25 14:39:51 +0100

    move: to-do comment

--- packages/ui-lib/src/components/Avatar/Avatar.tsx
+++ packages/ui-lib/src/components/Avatar/Avatar.tsx
@@ -4,6 +4,7 @@ import { Avatar as CHAvatar } from "@chakra-ui/react";
 const Avatar: typeof CHAvatar = (props) => {
   return (
     <CHAvatar
+      // to-do: animation on hover: avatar slides to the left, log-out icon in the background
       sx={{
         ":hover": {
           cursor: "pointer",
