050a4877ccb05ede5d9265b43a8b8671897b3424
Adrian Pilarczyk <adrian@useo.pl>
2021-03-25 14:00:46 +0100

    add: Google svg icon

--- packages/client/features/User/User.tsx
+++ packages/client/features/User/User.tsx
@@ -1,6 +1,7 @@
 import { signIn, signOut, useSession } from "next-auth/client";
 import React from "react";
 import { Avatar, Button } from "ui-lib";
+import Image from "next/image";
 
 const User: React.FC = () => {
   const [session, loading] = useSession();
@@ -17,7 +18,12 @@ const User: React.FC = () => {
           width="48px"
           onClick={() => signIn()}
         >
-          G
+          <Image
+            src="/icons/google.svg"
+            alt="Google logo"
+            width="15"
+            height="15"
+          />
         </Button>
       )}
       {session && (
